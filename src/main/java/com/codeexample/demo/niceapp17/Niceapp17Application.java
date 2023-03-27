7c7517a41e81d258005b79eead24afbe6d13b426
Izandro <izandromm@gmail.com>
2023-03-26 23:55:24 -0300

    files added to git repo.

---
+++ src/main/java/com/codeexample/demo/niceapp17/Niceapp17Application.java
@@ -0,0 +1,13 @@
+package com.codeexample.demo.niceapp17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Niceapp17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Niceapp17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeexample/demo/niceapp17/Niceapp17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeexample.demo.niceapp17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Niceapp17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
