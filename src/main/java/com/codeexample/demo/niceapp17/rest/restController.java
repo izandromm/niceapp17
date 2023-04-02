77438ecfce21d410d3898381806ef64aa6f18012
Izandro <izandromm@gmail.com>
2023-04-02 14:33:36 -0300

    27- 06- properties demo

--- src/main/java/com/codeexample/demo/niceapp17/rest/restController.java
+++ src/main/java/com/codeexample/demo/niceapp17/rest/restController.java
@@ -1,10 +1,24 @@
 package com.codeexample.demo.niceapp17.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class restController {
+
+    // inject properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     // expose "/" that return "Hello world"
 
